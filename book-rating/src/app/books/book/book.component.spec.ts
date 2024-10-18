36c84664465819eaf7cc10cd8d9547536ad38041
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-10-18 11:26:01 +0200

    fixture.componentRef.setInput

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { Book } from '../shared/book';
+import { signal } from '@angular/core';
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -14,6 +16,12 @@ describe('BookComponent', () => {
 
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // alter Stil (wenn Decorator)
+    // component.book = {} as Book;
+    // component.book = signal({} as Book)  // geht auch nicht
+    fixture.componentRef.setInput('book', {} as Book);
+
     fixture.detectChanges();
   });
 
