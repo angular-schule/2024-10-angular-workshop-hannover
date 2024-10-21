cca48ad73dd77e2555bdc25a6d62a31dae28f80c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-10-21 10:49:58 +0200

    ngrx: State

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZ
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { bookFeature } from './books/store/book.reducer';
+import { BookEffects } from './books/store/book.effects';
 
 
 export const appConfig: ApplicationConfig = {
@@ -14,8 +16,13 @@ export const appConfig: ApplicationConfig = {
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes /*, withDebugTracing()*/),
     provideHttpClient(),
+
     provideStore(),
     provideEffects(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-]
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+
+    // NEU
+    provideState(bookFeature),
+    provideEffects(BookEffects)
+  ]
 };
--- book-rating/src/app/books/store/book.reducer.ts
+++ book-rating/src/app/books/store/book.reducer.ts
@@ -1,14 +1,17 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { BookActions } from './book.actions';
+import { Book } from '../shared/book';
 
 export const bookFeatureKey = 'book';
 
 export interface State {
-
+  books: Book[],
+  loading: boolean
 }
 
 export const initialState: State = {
-
+  books: [],
+  loading: false
 };
 
 export const reducer = createReducer(
