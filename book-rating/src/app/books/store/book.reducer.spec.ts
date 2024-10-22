ff01a3d4fbc8e7398da52bd6ebd2ee9af852016e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-10-22 10:55:22 +0200

    Hands On: Book Reducer

--- book-rating/src/app/books/store/book.reducer.spec.ts
+++ book-rating/src/app/books/store/book.reducer.spec.ts
@@ -1,13 +1,37 @@
+import { Book } from '../shared/book';
+import { BookActions } from './book.actions';
 import { reducer, initialState } from './book.reducer';
 
 describe('Book Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
 
-      const result = reducer(initialState, action);
-
-      expect(result).toBe(initialState);
-    });
+  it('should set loading flag to `true` on loadBooks action', () => {
+    const action =  BookActions.loadBooks();
+    const newState = reducer(initialState, action);
+    expect(newState.loading).toBeTrue();
   });
+
+  it('should set books on loadBooksSuccess action', () => {
+
+    const newBooks: Book[] = [];
+    const action = BookActions.loadBooksSuccess({ books: newBooks });
+
+    const oldState = {
+      books: [],
+      loading: true
+    };
+
+    // ODER
+    // const oldState = reducer(initialState, BookActions.loadBooks());
+
+    const newState = reducer(oldState, action);
+
+    expect(newState.loading).toBeFalse();
+    expect(newState.books).toBe(newBooks);
+  });
+
+  // Hands On (Gruppenarbeit):
+
+  // 1. Teste den Reducer, der auf die create-Action reagiert
+  // 2. Teste den Reducer, der auf die rateUp-Action reagiert
+
 });
